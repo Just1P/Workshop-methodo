2318f213d4a535acd95f49524d57df4ce73d9c54
Pitra <justin.pitra@gmail.com>
2024-10-07 11:43:11 +0200

    TP 2

--- VendorMachine.ts
+++ VendorMachine.ts
@@ -5,8 +5,23 @@ class VendorMachine {
 
   private money;
 
+  public constructor() {
+    this.isOn = false;
+    this.snacksQty = 50;
+    this.money = 0;
+  }
+
   public buySnack() {
+    if ((this.isOn = false)) {
+      throw new Error("La machine est éteinte");
+    }
+
     this.isOn = true;
+
+    if ((this.snacksQty = 0)) {
+      this.reset;
+    }
+
     this.snacksQty -= 1;
     this.money += 2;
   }
@@ -19,9 +34,25 @@ class VendorMachine {
   }
 
   public shootWithFoot() {
+    if ((this.isOn = false)) {
+      throw new Error("La machine est éteinte");
+    }
+
     this.isOn = true;
+
+    if (this.snacksQty < 5) {
+      throw new Error("Il n'y a malheuresement plus de stock");
+    }
+
     this.snacksQty -= 5;
+
+    if (this.money < 20) {
+      throw new Error("bien essayé, mais y'a plus de sous");
+    }
+
     this.money -= 20;
     this.isOn = false;
   }
 }
+
+const vendorMachine = new VendorMachine();
